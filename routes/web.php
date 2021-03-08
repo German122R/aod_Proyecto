bd4c754e74cbf20efb1e837ba9bcf9e727c79429
German122R <l19te0522@teziutlan.tecnm.mx>
2021-03-08 17:57:42 -0600

    practica 3.0

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,6 @@ Route::resources([
 Route::resources([
     'xboxes'=> XboxController::class,
 ]);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
